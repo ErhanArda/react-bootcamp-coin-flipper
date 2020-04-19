f90cdce3d4bd137e1f9173fe474830271edbe4d6
Erhan Arda <erhanarda7@gmail.com>
2020-04-20 00:47:21 +0300

    Updates

---
+++ precache-manifest.3568411525eeb83ca2d82f53aa1fff88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea0a6aa85748467ed1655868f936eca",
+    "url": "/react-bootcamp-coin-flipper/index.html"
+  },
+  {
+    "revision": "1d13dbbe92194d720978",
+    "url": "/react-bootcamp-coin-flipper/static/css/main.f66e745e.chunk.css"
+  },
+  {
+    "revision": "947810643dd685c0d183",
+    "url": "/react-bootcamp-coin-flipper/static/js/2.739abd98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bootcamp-coin-flipper/static/js/2.739abd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d13dbbe92194d720978",
+    "url": "/react-bootcamp-coin-flipper/static/js/main.3cb71e0f.chunk.js"
+  },
+  {
+    "revision": "ca648e847c25d7712eca",
+    "url": "/react-bootcamp-coin-flipper/static/js/runtime-main.0823c2e0.js"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/react-bootcamp-coin-flipper/static/media/head.9b70882e.png"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/react-bootcamp-coin-flipper/static/media/tail.7c42669a.png"
+  }
+]);
